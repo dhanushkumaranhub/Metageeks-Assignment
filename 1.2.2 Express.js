44e8b754114a3ba3dd5ff1acb6bc8266b48b8cb1
Dhanush Kumaran <138612943+dhanushkumaranhub@users.noreply.github.com>
2023-11-28 23:38:31 +0530

    Update 1.2.2 Express.js

--- 1.2.2 Express.js
+++ 1.2.2 Express.js
@@ -1,1 +1,72 @@
 
+React.js State Management (5 points):
+
+● Implement state management in React to handle the loading state
+while fetching the blog posts. Show a loading indicator when posts are
+being fetched.
+
+
+prerequisites:
+
+npm install express mongoose
+
+
+js:
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB (replace 'your-mongodb-uri' with your actual MongoDB URI)
+mongoose.connect('your-mongodb-uri', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define the Post model
+const Post = mongoose.model('Post', {
+  title: String,
+  content: String,
+  author: String,
+});
+
+// Express route to retrieve a list of posts as JSON
+app.get('/posts', async (req, res) => {
+  try {
+    // Retrieve all posts from the database
+    const posts = await Post.find();
+
+    // Return the posts as JSON
+    res.json(posts);
+  } catch (error) {
+    console.error('Error retrieving posts:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+..........................................................................................................................................................................................
+
+
+Explanation:
+
+Database Connection:
+The code connects to MongoDB using Mongoose.
+  
+Post Model:
+The Post model is defined using Mongoose to represent the structure of posts in the database.
+  
+Express Route:
+The /posts route is defined to handle GET requests.
+Inside the route handler, it uses Post.find() to retrieve all posts from the database.
+  
+Error Handling:
+If there's an error during the database operation, it returns a 500 Internal Server Error with a JSON response.
+  
+Return JSON Response:
+If successful, it returns the retrieved posts as a JSON response.
