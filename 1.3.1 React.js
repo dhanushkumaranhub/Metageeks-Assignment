3efd1583fccf9f6b361ee1b7f8fea80f5dea89c1
Dhanush Kumaran <138612943+dhanushkumaranhub@users.noreply.github.com>
2023-11-28 23:09:11 +0530

    Update 1.3.1 React.js

--- 1.3.1 React.js
+++ 1.3.1 React.js
@@ -1,1 +1,8 @@
+          React.js Component (5 points):
+● Create a React.js functional component that displays a list of blog
+posts. Each post should show the title, author, and a button to view the
+full post.
 
+          here is my first project link :
+
+https://github.com/dhanushkumaranhub/React.js-Component.git
